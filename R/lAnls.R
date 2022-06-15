d1560854dbb8f399137472b6268bcd0d7a89a6b0
Elias Laurin Meyer <elias.meyer@meduniwien.ac.at>
2022-06-15 18:11:02 +0200

    FIx nMstn Problem

--- R/lAnls.R
+++ R/lAnls.R
@@ -217,15 +217,10 @@ lAnls <- function(
       results <- match.fun(analysis_function)(analysis_data)
       
       # Save Analysis Dataset as well
-      lPltfTrial$isa[[lAddArgs$current_id]]$lAnalyses <- 
-        c(
-          lPltfTrial$isa[[lAddArgs$current_id]]$lAnalyses,
-          list(
-            list(
-              results = results,
-              analysis_data = analysis_data
-            )
-          )
+      lPltfTrial$isa[[lAddArgs$current_id]]$lAnalyses[[lAddArgs$nMstn]] <- 
+        list(
+          results = results,
+          analysis_data = analysis_data
         )
       
       print(
