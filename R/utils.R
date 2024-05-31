90923f4a6efa14eb3d89f08efe5b14f1e31e5160
Elias Laurin Meyer <elm123@protonmail.ch>
2024-05-31 14:42:00 +0200

    Re-organise the package

---
+++ R/utils.R
@@ -0,0 +1,27 @@
+
+.onAttach <- function(libname, pkgname) {
+  
+  v <- getNamespaceVersion("simple")
+  rule <- paste0(rep("-", getOption("width")), collapse = "")
+  
+  packageStartupMessage(rule)
+  packageStartupMessage(paste0("simple ", v, " loaded."))
+  packageStartupMessage("Visit for more information: ")
+  packageStartupMessage("https://github.com/el-meyer/simple/")
+  packageStartupMessage(rule)
+  packageStartupMessage("Don't forget to report bugs and request features under:")
+  packageStartupMessage("https://github.com/el-meyer/simple/issues")
+  packageStartupMessage(rule)
+  
+}
+
+
+.onDetach <- function(libpath) {
+  
+  rule <- paste0(rep("-", getOption("width")), collapse = "")
+  packageStartupMessage(rule)
+  packageStartupMessage("Thank you for using the simple package!")
+  packageStartupMessage("Don't forget to report bugs and request features under:")
+  packageStartupMessage("https://github.com/el-meyer/simple/issues")
+  packageStartupMessage(rule)
+}
